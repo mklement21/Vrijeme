78dc2b6f7f1c9c14e7ab47daccf5725e6b5df5b2
Matea Klement <mklement21@foi.hr>
2024-05-24 23:13:56 +0200

    Get API data based on user input of city name in search element.

--- app/src/main/java/com/example/vrijeme/WeatherActivity.kt
+++ app/src/main/java/com/example/vrijeme/WeatherActivity.kt
@@ -2,6 +2,8 @@ package com.example.vrijeme
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.vrijeme.helpers.RetrofitInstance
@@ -11,14 +13,24 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class WeatherActivity : ComponentActivity() {
+    private lateinit var searchCity: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
 
         val apiKey = "df70babbb87f5d879808ee33ec5685f6"
-        val city = "Zagreb"
 
-        fetchData(city, apiKey)
+        searchCity = findViewById(R.id.searchCity)
+
+        val searchButton = findViewById<Button>(R.id.searchButton)
+        searchButton.setOnClickListener {
+            val city = searchCity.text.toString().trim()
+            if (city.isNotEmpty()) {
+                fetchData(city, apiKey)
+            } else {
+                showToast("Enter city name")
+            }
+        }
     }
 
     private fun fetchData(city: String, apiKey: String) {
