654bd901ffbd1888cb9f234c04e16e48d9982571
Matea Klement <mklement21@foi.hr>
2024-05-25 19:57:46 +0200

    Created WeatherAtributesData for weather attributes recyclerview.

---
+++ app/src/main/java/com/example/vrijeme/adapters/WeatherAttributeAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.vrijeme.adapters
+
+class WeatherAttributeAdapter {
+}
---
+++ app/src/main/java/com/example/vrijeme/classes/WeatherAtributesData.kt
@@ -0,0 +1,10 @@
+package com.example.vrijeme.classes
+
+data class WeatherAtributesData(
+    val pressure: Int,
+    val humidity: Int,
+    val windSpeed: Double,
+    val windDegree: Int,
+    val clouds: Int,
+    val visibility: Int
+)
