22fcb83fbfd3a6e0c91ddb932aa449c9b5b7f2db
Matea Klement <mklement21@foi.hr>
2024-06-05 20:22:58 +0200

    Initialization of LocationForegroundService.

---
+++ app/src/main/java/com/example/vrijeme/services/LocationForegroundService.kt
@@ -0,0 +1,18 @@
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class LocationForegroundService : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
