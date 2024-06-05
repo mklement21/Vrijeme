d7cc7924296c60c32d2757a48db63a7e4a23f406
Matea Klement <mklement21@foi.hr>
2024-06-05 20:42:34 +0200

    Initialization of WeatherNotificationService.

---
+++ app/src/main/java/com/example/vrijeme/services/WeatherNotificationService.kt
@@ -0,0 +1,9 @@
+package com.example.vrijeme.services
+
+import android.app.IntentService
+import android.content.Intent
+
+class WeatherNotificationService : IntentService("WeatherNotificationService") {
+    override fun onHandleIntent(intent: Intent?) {
+    }
+}
